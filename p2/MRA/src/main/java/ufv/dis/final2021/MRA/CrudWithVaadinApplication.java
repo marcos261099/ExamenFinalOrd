d58dc61913522d7f5b65e18c6b7bf9c9a5374325
Marcos Rodriguez(Avances) <marcos261099>
2021-01-29 19:06:57 +0100

    añadimos ips

--- p2/MRA/src/main/java/ufv/dis/final2021/MRA/CrudWithVaadinApplication.java
+++ p2/MRA/src/main/java/ufv/dis/final2021/MRA/CrudWithVaadinApplication.java
@@ -28,29 +28,7 @@ public class CrudWithVaadinApplication {
             repository.save(new Customer("Miguel", "Santos"));
             repository.save(new Customer("Antonio", "Wells"));
 
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (Customer customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
 
-            // fetch an individual customer by ID
-            Customer customer = repository.findById(1L).get();
-            log.info("Customer found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastNameStartsWithIgnoreCase('Wells'):");
-            log.info("--------------------------------------------");
-            for (Customer bauer : repository
-                    .findByLastNameStartsWithIgnoreCase("Wells")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
         };
     }
 
---
+++ p2/MRA/src/main/java/ufv/dis/final2021/MRA/IPS.java
@@ -0,0 +1,59 @@
+package ufv.dis.final2021.MRA;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class IPS {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long ip_from;
+
+    private Long ip_to;
+
+    @Column(length=2)
+    private String country_code;
+
+    @Column(length=64)
+    private String country_name;
+
+    @Column(length=128)
+    private String region_name;
+
+    @Column(length=128)
+    private String city_name;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @Column(length=30)
+    private String zip_code;
+
+    @Column(length=8)
+    private String time_zone;
+
+    protected IPS() {
+    }
+
+    public IPS(Long ip_from,Long ip_to, String country_code,String country_name,String region_name,String city_name,Double latitude,Double longitude,String zip_code,String time_zone) {
+    this.ip_from=ip_from;
+    this.ip_to=ip_to;
+    this.country_code=country_code;
+    this.country_name=country_name;
+    this.region_name=region_name;
+    this.city_name=city_name;
+    this.latitude=latitude;
+    this.longitude=longitude;
+    this.zip_code=zip_code;
+    this.time_zone=time_zone;
+    }
+
+
+
+}
