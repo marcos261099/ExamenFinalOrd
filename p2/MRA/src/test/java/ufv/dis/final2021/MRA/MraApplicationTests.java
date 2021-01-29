faec29ac880390d86d4e8cb5bd26157762b330bc
Marcos Rodriguez(Avances) <marcos261099>
2021-01-29 18:49:44 +0100

    Empezamos con la parte 2

---
+++ p2/MRA/src/test/java/ufv/dis/final2021/MRA/MraApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2021.MRA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
