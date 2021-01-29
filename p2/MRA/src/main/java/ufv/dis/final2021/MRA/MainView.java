fca1af6bd04488b9efb3cfbe30a7a201754b067c
Marcos Rodriguez(Avances) <marcos261099>
2021-01-29 19:47:10 +0100

    limpiamos algunas clases

--- p2/MRA/src/main/java/ufv/dis/final2021/MRA/MainView.java
+++ p2/MRA/src/main/java/ufv/dis/final2021/MRA/MainView.java
@@ -14,66 +14,13 @@ import org.springframework.util.StringUtils;
 @Route
 public class MainView extends VerticalLayout {
 
-    private final CustomerRepository repo;
 
-    private final CustomerEditor editor;
 
-    final Grid<Customer> grid;
+    private final Button comprobar;
 
-    final TextField filter;
-
-    private final Button addNewBtn;
-
-    public MainView(CustomerRepository repo, CustomerEditor editor) {
-        this.repo = repo;
-        this.editor = editor;
-        this.grid = new Grid<>(Customer.class);
-        this.filter = new TextField();
-        this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
-
-        // build layout
-        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-        add(actions, grid, editor);
-
-        grid.setHeight("300px");
-        grid.setColumns("id", "firstName", "lastName");
-        grid.getColumnByKey("id").setWidth("50px").setFlexGrow(0);
-
-        filter.setPlaceholder("Filter by last name");
-
-        // Hook logic to components
-
-        // Replace listing with filtered content when user changes filter
-        filter.setValueChangeMode(ValueChangeMode.EAGER);
-        filter.addValueChangeListener(e -> listCustomers(e.getValue()));
-
-        // Connect selected Customer to editor or hide if none is selected
-        grid.asSingleSelect().addValueChangeListener(e -> {
-            editor.editCustomer(e.getValue());
-        });
-
-        // Instantiate and edit new Customer the new button is clicked
-        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
-
-        // Listen changes made by the editor, refresh data from backend
-        editor.setChangeHandler(() -> {
-            editor.setVisible(false);
-            listCustomers(filter.getValue());
-        });
-
-        // Initialize listing
-        listCustomers(null);
+    public MainView(IpsBBDD repo) {
+        this.comprobar = new Button("New customer", VaadinIcon.PLUS.create());
     }
 
-    // tag::listCustomers[]
-    void listCustomers(String filterText) {
-        if (StringUtils.isEmpty(filterText)) {
-            grid.setItems(repo.findAll());
-        }
-        else {
-            grid.setItems(repo.findByLastNameStartsWithIgnoreCase(filterText));
-        }
-    }
-    // end::listCustomers[]
 
 }
